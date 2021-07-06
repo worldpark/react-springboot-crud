348d0a2892111a2a3c3dfe648b45bc56a20b3dd0
worldpark <vbwjsdl@naver.com>
2021-07-06 13:38:49 +0900

    react / spring boot
    react / spring boot

--- demo/src/main/java/com/example/demo/mapper/BoardMapper.java
+++ demo/src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.vo.BoardVO;
-import com.example.demo.vo.ReplyVO;
 
 @Mapper
 public interface BoardMapper {
